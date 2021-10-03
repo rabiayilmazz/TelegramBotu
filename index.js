92ae59d45ba2cc55eba48d9982639bb5d0af8dc4
00101111 <12167194+00101111@users.noreply.github.com>
2021-10-03 15:53:35 +0300

    Update index.js
    Fix

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
 //**********************************
-	// Api eklenir.
-	var telegram = require('telegram-bot-api');	
+	// API eklenir.
+	const telegram = require('telegram-bot-api')
 
-	// Apimize gerekli JSON yollanır.
-	var api = new telegram({
-		token: 'TOK:EN',
+	// Token eklenir.
+	const BOT_TOKEN = 'TOK:EN';
+
+	// API'ye gerekli bilgiler JSON ile yollanır.
+	const api = new telegram({
+		token: BOT_TOKEN,
 		updates: {
 			enabled: true,
 			get_interval: 1000
 		}
-	});
+	})
 
-	// "Message" gelirse çalışmaısnı istediğimiz fonksiyonu yazıyoruz.
-	api.on('message', function(message) {
-		if (message.text != "" && message.text != undefined) {
-			var response = "Sen : " + message.text + " içerikli mesaj attın!";  // Gelen mesajı göndereceğimiz string.
-			
+	// Mesaj sağlayıcısı tanımlanır.
+	const mp = new telegram.GetUpdateMessageProvider()
+
+	// Mesaj sağlayıcısı belirtilir ve API başlatılır.
+	api.setMessageProvider(mp)
+	api.start()
+	.then(() => {
+		console.log('Bot başlatıldı.')
+	})
+	.catch(console.err) // Hata varsa yazdırılır.
+
+	// "Message" gelirse çalışmasını istediğimiz fonksiyonu yazıyoruz.
+	api.on('update', (update) => {
+		const message = update.message.text;
+
+		if (message != "" && message != undefined) {
+
+			var response = "Sen : " + message + " içerikli mesaj attın!"; // Gelen mesajı göndereceğimiz string.
 			// Gelen chat id'yi okuyoruz ilgili chat id ye gitsin diyoruz. A grubundan okunan mesajın
 			// B grubuna gitmesini istemeyiz :)
+			const chat_id = update.message.chat.id
+
 			// reply_to_message_id ise reply olarak cevaplamak için ekliyoruz.
 			api.sendMessage({
-				reply_to_message_id:message.message_id,
-				chat_id: message.chat.id,
-				text: response
+					chat_id: chat_id,
+					reply_to_message_id: update.message.message_id,
+					text: response
 			})
-			.then(function(message)
-			{
+			.then(() => {
 				// Mesaj gönderildikten sonra. veya bilgi alma da
+				console.log("Mesaj başarıyla yollandı.");
 			})
-			.catch(function(err)
-			{
-				console.log(err);
-			});
+			.catch(console.err)
 		}
 	});
